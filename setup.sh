e512388eab52502919e88de13244d99aa89fe165
extremecoders-re <extremecoders@hotmail.com>
2022-08-19 10:57:04 +0530

    Add files

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -e
+
+show_usage() {
+  echo "Usage: $(basename $0) takes exactly 1 argument (install | uninstall)"
+}
+
+if [ $# -ne 1 ]
+then
+  show_usage
+  exit 1
+fi
+
+check_env() {
+  if [[ -z "${APM_TMP_DIR}" ]]; then
+    echo "APM_TMP_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${APM_PKG_INSTALL_DIR}" ]]; then
+    echo "APM_PKG_INSTALL_DIR is not set"
+    exit 1
+  
+  elif [[ -z "${APM_PKG_BIN_DIR}" ]]; then
+    echo "APM_PKG_BIN_DIR is not set"
+    exit 1
+  fi
+}
+
+install() {
+  wget https://github.com/garywill/linux-router/raw/9e1d985623e92a507c6eba93e0bfddb25e3e6f01/lnxrouter -O $APM_TMP_DIR/lnxrouter
+  mv $APM_TMP_DIR/lnxrouter $APM_PKG_INSTALL_DIR/
+  chmod +x $APM_PKG_INSTALL_DIR/lnxrouter
+  ln -s $APM_PKG_INSTALL_DIR/lnxrouter $APM_PKG_BIN_DIR/
+}
+
+uninstall() {
+  rm $APM_PKG_BIN_DIR/lnxrouter
+}
+
+run() {
+  if [[ "$1" == "install" ]]; then 
+    install
+  elif [[ "$1" == "uninstall" ]]; then 
+    uninstall
+  else
+    show_usage
+  fi
+}
+
+check_env
+run $1
